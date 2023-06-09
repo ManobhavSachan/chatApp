7db3a71510172b092edfe89ede79b33ab7a67fec
ManobhavSachan <94208034+ManobhavSachan@users.noreply.github.com>
2023-06-09 08:23:10 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import Constants from 'expo-constants';
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCtN0IA0mCP_TNV437xjNr5NC6MpFTw1Uw",
-  authDomain: "chatapp-95c62.firebaseapp.com",
-  projectId: "chatapp-95c62",
-  storageBucket: "chatapp-95c62.appspot.com",
-  messagingSenderId: "91636811772",
-  appId: "1:91636811772:web:ade76d6d0af81d069bd479",
-  measurementId: "G-EXDFVFKKD2"
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  databaseURL: Constants.manifest.extra.databaseURL
 };
 // initialize firebase
 initializeApp(firebaseConfig);
